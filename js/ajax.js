84f7a2dd450059a1f0be94ff0e1e05c4fc17929a
Marcelo de Carvalho Santana <marcelo.scientist@hotmail.com>
2020-05-10 10:18:49 -0300

    Some refactoring done

--- js/ajax.js
+++ js/ajax.js
@@ -4,7 +4,6 @@
   /* 
     pagination settings
   */
-
   const app = document.getElementById('root');
   const source = "../assets/img/@home.jpg";
   const logo = document.createElement('img');
@@ -15,19 +14,13 @@
 
   app.appendChild(logo);
   app.appendChild(container);
-
-
   /* 
     pagination settings
   */
-
   const first = document.querySelector(".first");
   const previous = document.querySelector(".previous");
   const next = document.querySelector(".next");
   const last = document.querySelector(".last");
-
-
-
   
  
 var request = new XMLHttpRequest()
@@ -36,80 +29,78 @@ request.onload = function() {
   // Begin accessing JSON data here
   var houses = JSON.parse(this.response)
   if (request.status >= 200 && request.status < 400) { 
-
-
     /* CREATING THE CARDS */
     let page = 0;
     let cards = []
+    
+    createCards(cards, houses)
+    displayBlock(page, cards)
 
-    for (let index = 0; index < houses.length; index++) {
-      let house = houses[index]
-      const card = document.createElement('div')
-      card.setAttribute('class', 'card')
-
-      const img = document.createElement('img')
-      img.src = house.photo
-
-      const h1 = document.createElement('h1')
-      h1.textContent = house.property_type
-
-      const p = document.createElement('p')
-      house.name = house.name.substring(0, 300)
-      p.textContent = `${house.name}...`
-
-      container.appendChild(card)
-      card.appendChild(img)
-      card.appendChild(h1)
-      card.appendChild(p)
-      card.style.display = 'none';
-      cards.push(card);
+    const btnAction = (btnName,  page) => {
+      btnName.addEventListener('click', ()=>{
+        console.log(page)
+        container.innerHTML = "";
+        displayBlock(page, cards)
+      })
     }
+    btnAction(first,  page = 0)
+    btnAction(last,  page = cards.length - 3)
 
-    for (let index = 0; index < page+3; index++) {
-      cards[index].style.display = 'block'
-      container.appendChild(cards[index]);
-    }
-
-    first.addEventListener('click', ()=>{
-      page = 0;
-      container.innerHTML = "";
-      for (let index = page; index < page+3; index++) {
-        cards[index].style.display = 'block'
-        container.appendChild(cards[index]);
-      }
-    })
-  
-    last.addEventListener('click', ()=>{
-      page = cards.length - 3;
-      container.innerHTML = "";
-      for (let index = page; index < page+3; index++) {
-        cards[index].style.display = 'block'
-        container.appendChild(cards[index]);
-      }
-    })
   
     next.addEventListener('click', ()=>{
       page == cards.length - 3? (page = 0) : (page += 3);
+      console.log(page)
       container.innerHTML = "";
-      for (let index = page; index < page+3; index++) {
-        cards[index].style.display = 'block'
-        container.appendChild(cards[index]);
-      }
+      displayBlock(page, cards)
     })
   
     previous.addEventListener('click', ()=>{
       page == 0 ? (page = cards.length - 3) : (page -= 3);
+      console.log(page)
       container.innerHTML = "";
-      for (let index = page; index < page+3; index++) {
-        cards[index].style.display = 'block'
-        container.appendChild(cards[index]);
-      }
+      displayBlock(page, cards)
     })
+
   } else {
     const errorMessage = document.createElement('marquee')
     errorMessage.textContent = `Gah, it's not working!`
     app.appendChild(errorMessage)
   }
+}
+
+
+
+
+const displayBlock = (page, cards) => {
+  for (let index = page; index < page+3; index++) {
+    cards[index].style.display = 'block'
+    container.appendChild(cards[index]);
+  }  
+}
+
+const createCards = (cards, houses) => {
+  for (let index = 0; index < houses.length; index++) {
+    let house = houses[index]
+    const card = document.createElement('div')
+    card.setAttribute('class', 'card')
+
+    const img = document.createElement('img')
+    img.src = house.photo
+
+    const h1 = document.createElement('h1')
+    h1.textContent = house.property_type
+
+    const p = document.createElement('p')
+    house.name = house.name.substring(0, 300)
+    p.textContent = `${house.name}...`
+
+    container.appendChild(card)
+    card.appendChild(img)
+    card.appendChild(h1)
+    card.appendChild(p)
+    card.style.display = 'none';
+    cards.push(card);
+  }  
 }
 
 request.send()
